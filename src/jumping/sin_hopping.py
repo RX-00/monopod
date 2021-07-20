d3d7eb75b41e63edcb53453ecc0ab825d72ca5db
RX-00 <00royxing@gmail.com>
2021-07-20 14:03:25 -0400

    redid the control loop, maybe use a square wave instead

--- src/jumping/sin_hopping.py
+++ src/jumping/sin_hopping.py
@@ -16,6 +16,7 @@ import sys
 
 # TODO: check out and if needed tune/change the motor's internal PID
 #       in order to increase damping!
+# TODO: setup the links and coordinates to be in meters
 
 
 async def main():
@@ -23,10 +24,10 @@ async def main():
     # NOTE: sin controller: Kp,  dt,   l0,  l1,  animation)
     ctrlr = sinIkHopCtrlr(25.0, 0.015, 1.0, 1.2, False)
 
-
     knee = 1
     hip = 2
     theta_hp = theta_kn = 0
+    wave = 0
 
     # create the leg class
     monopod = Leg(knee, hip) # NOTE: knee = 1, hip = 2
@@ -38,19 +39,34 @@ async def main():
     while True:
         now = time.time()
 
+        #if (wave > 0):
+        #    ctrlr.q[0] = 0.0
+        #    ctrlr.q[1] = 0.0
+        #elif (wave < 0):
+        #    ctrlr.q[0] = 0.0
+        #    ctrlr.q[1] = 0.0
+
+
+        # desired pos:
+        ctrlr.q[0] = 0.0
+        ctrlr.q[1] = 1.0 * np.sin(now) - 2.0
+        # NOTE TODO: if these desired positions don't work out
+        #            make it so you create a square wave instead
+        #wave = np.sin(now / 1.0)
+
         theta_hp, theta_kn = ctrlr.two_link_leg_ik(
             des_eps=0.1, theta0=theta_hp, theta1=theta_kn)
         hp_pos = ctrlr.convert_rad_enc_hp(theta_hp)
         kn_pos = ctrlr.convert_rad_enc_kn(theta_kn)
 
-        await monopod.set_motor_kn_cmds(math.nan, # pos
+        await monopod.set_motor_kn_cmds(kn_pos, # pos
                                         math.nan, # vel
                                         2.0,      # max_torque
                                         math.nan, # stop_pos
                                         -0.01,    # ffwd_torque
                                         math.nan, # watchdog_timeout
                                         True)     # query
-        await monopod.set_motor_hp_cmds(math.nan,
+        await monopod.set_motor_hp_cmds(hp_pos,
                                         math.nan,
                                         2.0,
                                         math.nan,
